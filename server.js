1c7f5f19f991377448ce1e2a472c742bccd94634
Dani9730 <dgonzalezc2@est.ups.edu.ec>
2019-12-18 12:33:46 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+//declara una variable app que es el producto de la funcion express
+const app = express();
+const axios = require('axios');
+
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+//todas las peticiones que entren con / van a ejecutar esta funcion
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Daniela'
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
+
+app.get('/latitud/:latitud/longitud/:longitud', (req, res) => {
+    console.log(req.params.latitud, req.params.longitud);
+    res.end('recibido')
+})
