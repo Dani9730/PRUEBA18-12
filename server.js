5287e44a3663a83422a00d04be26a24cdab6629f
Dani9730 <dgonzalezc2@est.ups.edu.ec>
2019-12-18 13:21:54 -0500

    doooos

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ require('./hbs/helpers');
 
 const port = process.env.PORT || 3000;
 
-const argv = require('yargs').options({
-    nombre: {
-        alias: 'n',
-        desc: 'Nombre de la ciudad para obtener el clima',
-        demand: true
-    }
-}).argv;
+
 app.use(express.static(__dirname + '/public'));
 // Express HBS engine
 hbs.registerPartials(__dirname + '/views/parciales');
@@ -45,17 +39,3 @@ app.get('/latitud/:latitud/longitud/:longitud', (req, res) => {
     console.log(req.params.latitud, req.params.longitud);
     res.end('recibido')
 });
-
-const getInfo = async(ciudad) => {
-    try {
-        const coords = await ubicacion.getCiudadLatLon(ciudad);
-        const temp = await clima.getClima(coords.lat, coords.lng);
-        return `El clima de ${ coords.direccion } es de ${ temp }.`;
-    } catch (e) {
-        return `No se pudo determinar el clima de ${ ciudad }`;
-    }
-}
-
-getInfo(argv.nombre)
-    .then(console.log)
-    .catch(console.log);
